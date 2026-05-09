f334c6da885ecb3fb8016c6c65a7b0cc7dfe7ba8
tadeskops <ta.deskops@gmail.com>
2026-05-09 23:43:50 +0530

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,7 +3,7 @@
 
 const API = {
     // IMPORTANT: Replace with your Apps Script Web App deployment URL
-    ENDPOINT: "https://script.google.com/macros/d/AKfycbw7_-1UDpqmfnOY0wMObADYRqpyIHxY2-HmA2RhFX2NuaxDyuKNRPb0BgUJ2XFVhue_iw/userweb/v1/execute",
+    ENDPOINT: "https://script.google.com/macros/d/AKfycby0Cr0oUusEAQp0RaTPhvdrYfzVI2GfNXFbJuwggOKbZpmNHjiGRh-ZSxGeUh-I8MzsvQ/userweb/v1/execute",
     
     // Set this after deployment
     setEndpoint(url) {
